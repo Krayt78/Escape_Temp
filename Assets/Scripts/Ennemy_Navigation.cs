19e54181acff04ff80191c199a212d3cd8545b21
Ludovic Domingues <ludovic.domingues96@gmail.com>
2020-01-24 09:18:47 +0100

    Began work on AI

---
+++ Assets/Scripts/Ennemy_Navigation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Ennemy_Navigation : MonoBehaviour
+{
+
+    private NavMeshAgent m_navMeshAgent;
+
+    //Serialized Fields
+    [SerializeField]
+    private GameObject target;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+        m_navMeshAgent.Warp(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+            m_navMeshAgent.SetDestination(target.transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+            target = other.gameObject;
+    }
+}
