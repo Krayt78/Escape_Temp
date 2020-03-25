6ebd30e4c47c2523efc9c64612b57a965c3f6a98
SullimanAbdul <SullimanAbdul@users.noreply.github.com>
2020-03-25 10:28:45 +0100

    grapplin is now fully working

--- Assets/Scripts/Grapplin.cs
+++ Assets/Scripts/Grapplin.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,12 @@ public class Grapplin : MonoBehaviour
 {
 
     private PlayerInput playerInput;
+
+    public LineRenderer lrRope;
+    private int nbPoints = 2;
+    //private Vector3[] positions = new Vector3[50];
+
+    public Transform grapplinPosition;
 
     [SerializeField] Transform playerCamera;
 
@@ -16,15 +23,24 @@ public class Grapplin : MonoBehaviour
     bool coroutine = false;
 
     Rigidbody m_rigibody;
-    Vector3 destination = new Vector3();
+    private Vector3 destination = new Vector3();
     Vector3 bezierControlPoint = new Vector3();
     
     Coroutine MoveCoroutine;
 
     float time = 0;
 
+    [SerializeField]
+    GameObject m_grapplinPoint;
+
+    GameObject grp;
+
+
+
     private void Awake()
     {
+        lrRope.positionCount = nbPoints;
+        lrRope.enabled = false;
         m_rigibody = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
         playerInput.OnGrapplin += UseGrapplin;
@@ -33,18 +49,21 @@ public class Grapplin : MonoBehaviour
     void Update()
     {
         //Cursor.visible = true;
-        CheckPosition();
+        CheckPosition();   
     }
+
     private void FixedUpdate()
     {
         ray = new Ray(playerCamera.position, playerCamera.forward);
         Debug.DrawRay(ray.origin, ray.direction * 10000f, Color.red);
-        //UseGrapplin();
     }
     private void CheckPosition()
     {
         if (Landed())
         {
+            //StopCoroutine(LaunchGrapplin(new GameObject()));
+            StopCoroutine(MoveOnBezier());
+            
             hitGrap = false;
             m_rigibody.constraints = RigidbodyConstraints.None;
             m_rigibody.freezeRotation = true;
@@ -53,20 +72,16 @@ public class Grapplin : MonoBehaviour
 
     private bool Landed()
     {
-
         return Vector3.Distance(transform.position,destination)<0.1f;
     }
 
     void UseGrapplin()
     {
-
         Debug.Log("We launch grapplin");
         ray = new Ray(playerCamera.position, playerCamera.forward);
         Debug.DrawRay(ray.origin, ray.direction * 10000f, Color.red);
         if (Physics.Raycast(ray, out hit))
         {
-            
-
             destination = hit.collider.gameObject.GetComponentInChildren<BezierPoint>().transform.position;
             //Debug.Log(destination);
             bezierControlPoint = destination;
@@ -76,46 +91,21 @@ public class Grapplin : MonoBehaviour
             time = 0;
         }
         if (hitGrap)
-        {
-            if (!coroutine)
-            {
-                m_rigibody.constraints = RigidbodyConstraints.FreezePositionY;
-                m_rigibody.freezeRotation = true;
-                
-                CreateBezier(destination, bezierControlPoint);
-            }
-            // StartCoroutine( MoveOnBezier());
-            //camera.transform.position = Vector3.MoveTowards(camera.transform.position, hit.point, Time.deltaTime*travelingSpeed);  
+        { 
+            grp = Instantiate(m_grapplinPoint, grapplinPosition.position,new Quaternion(), transform);
+            grp.transform.parent = null;
+            StartCoroutine(LaunchGrapplin(grp));
+
+             // StartCoroutine( MoveOnBezier());
+             //camera.transform.position = Vector3.MoveTowards(camera.transform.position, hit.point, Time.deltaTime*travelingSpeed);  */
         }
-
-    }
-
-    private void CreateBezier(Vector3 destination, Vector3 bezierCP)
-    {
-       
-        StartCoroutine(MoveOnBezier()); 
-    }
-
-    private IEnumerator MoveOnBezier()
-    {
-        coroutine = true;
-
-        for (float i = 0; i <= 1; i += 0.02f)
-        {
-            MoveCoroutine = StartCoroutine(MoveAlongBezier(i));
-            yield return MoveCoroutine;
-        }
-        // hitGrap = false;
-
-        Debug.Log("we finished");
     }
 
     void OnCollisionEnter(Collision col)
     {
         hitGrap = false;
         m_rigibody.constraints = RigidbodyConstraints.None;
-        m_rigibody.freezeRotation = true;
-       
+        m_rigibody.freezeRotation = true;    
     }
 
     private Vector3 CalculateBezierPoint(float time, Vector3 pos0, Vector3 pos1, Vector3 pos2)
@@ -129,14 +119,54 @@ public class Grapplin : MonoBehaviour
         return p;
     }
 
+    private void CreateBezier(Vector3 destination, Vector3 bezierCP)
+    {
+        StartCoroutine(MoveOnBezier());
+    }
+
     IEnumerator MoveAlongBezier(float t)
     {
         yield return new WaitForSecondsRealtime(0.03f);
         Debug.Log(t);
         transform.position = CalculateBezierPoint(t, transform.position, bezierControlPoint, destination);
         //camera.transform.LookAt(CalculateBezierPoint(t, transform.position, bezierControlPoint, destination));
-
-
         //yield return null;
+    }
+
+    private IEnumerator MoveOnBezier()
+    {
+        m_rigibody.constraints = RigidbodyConstraints.FreezePositionY;
+        m_rigibody.freezeRotation = true;
+        coroutine = true;
+        float y = transform.position.y;
+        for (float i = 0; i <= 1; i += 0.002f)
+        {
+            if (y > transform.position.y)
+            {
+                i += 0.02f;
+                lrRope.enabled = false;
+                Destroy(grp);
+            }
+            MoveCoroutine = StartCoroutine(MoveAlongBezier(i));
+            y = transform.position.y;
+            lrRope.SetPosition(0, transform.position);
+            yield return MoveCoroutine;
+        }
+        // hitGrap = false
+        Debug.Log("we finished");
+    }
+
+    private IEnumerator LaunchGrapplin(GameObject grp)
+    {
+        lrRope.enabled = true;
+        lrRope.SetPosition(0, transform.position);
+        while (grp.transform.position != hit.point)
+        {
+            grp.transform.position = Vector3.MoveTowards(grp.transform.position, hit.point, 10f * Time.deltaTime);
+            lrRope.SetPosition(1, grp.transform.position);
+            yield return null;
+        }
+        CreateBezier(destination, bezierControlPoint);
+        yield return null;
     }
 }
