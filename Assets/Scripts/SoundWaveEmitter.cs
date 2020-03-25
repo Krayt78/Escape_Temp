bdeff4fffc89cd32b92739bfbe2b1d38c69083db
NolanGougeon <gnolan@hotmail.fr>
2020-03-25 17:49:47 +0100

    Création du shader d'emission de bruit

---
+++ Assets/Scripts/SoundWaveEmitter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+public class SoundWaveEmitter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject soundWave;
+    private float lastTimeInstatiated;
+
+    private void Start()
+    {
+        lastTimeInstatiated = Time.time;
+        GetComponent<NoiseReceiver>().OnNoiseReceived += PulseWaveActivate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void PulseWaveActivate(Noise n)
+    {
+        Debug.Log("Test");
+        if (lastTimeInstatiated + Constants.INTERVAL_DURATION <= Time.time)
+        {
+            GameObject soundwaveInstantiated = Instantiate(soundWave, n.emissionPosition, Quaternion.identity);
+            soundwaveInstantiated.GetComponent<NoiseWave>().UpdateScale(n.range);
+
+            lastTimeInstatiated = Time.time;
+        }
+    }
+}
