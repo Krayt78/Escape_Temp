89d5507080ec798a3960c42b3e5d4ddf8dbf662b
NolanGougeon <gnolan@hotmail.fr>
2020-06-19 18:40:26 +0200

    Mouvement d'oeil random

--- Assets/Scripts/Enemy/EnemyEyeMovement.cs
+++ Assets/Scripts/Enemy/EnemyEyeMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyEyeMovement : MonoBehaviour
 {
@@ -11,9 +12,16 @@ public class EnemyEyeMovement : MonoBehaviour
     Vector3 originalRotation;
     Vector3 currentTarget;
     bool moveEye;
+
+    private float ElapsedTime = 0.0f;
+    private Quaternion randomRotation;
+    private Quaternion lastRotation;
+
     private void Start()
     {
         originalRotation = sentinelEye.localPosition;
+        randomRotation = Quaternion.Euler(Random.Range(-10.0f, 10.0f), Random.Range(-50.0f, -120.0f), 0);
+        lastRotation = randomRotation;
         moveEye = false;
     }
     public void MoveEyeAtTarget(Vector3 target)
@@ -26,15 +34,29 @@ public class EnemyEyeMovement : MonoBehaviour
         moveEye = false;
     }
 
-    public void MoveEyeAtOriginalPosition()
-    {
-        sentinelEye.localRotation = Quaternion.LookRotation(sentinelEye.localPosition);
-    }
     private void LateUpdate()
     {
         if (moveEye)
         {
             sentinelEye.LookAt(currentTarget);
+        }
+        else
+        {
+            MoveEyeRandomly();
+        }
+    }
+
+    public void MoveEyeRandomly()
+    {
+        float TotalTime = 2.0f;
+
+        ElapsedTime += Time.deltaTime;
+        lastRotation = Quaternion.Slerp(lastRotation, randomRotation, (ElapsedTime / TotalTime));
+        sentinelEye.localRotation = lastRotation;
+        if (ElapsedTime > TotalTime)
+        {
+            randomRotation = Quaternion.Euler(Random.Range(-10.0f, 10.0f), Random.Range(-50.0f, -120.0f), 0);
+            ElapsedTime = 0.0f;
         }
     }
 }
