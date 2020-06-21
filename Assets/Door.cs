4663e64e1e1a756d1841db93d1e0449443fd2c47
sulliman abdul <sulliman.abdul@gmail.com>
2020-06-21 23:04:44 +0200

    blocking tutorial first step

---
+++ Assets/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Transform doorTransform;
+    Vector3 originalPos;
+
+    public void Start()
+    {
+        doorTransform = GetComponentInChildren<GameObject>().transform;
+        originalPos = doorTransform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            doorTransform.position = new Vector3(doorTransform.position.x, doorTransform.position.y + 40, doorTransform.transform.position.z);
+        }
+       
+
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            doorTransform.position = new Vector3(doorTransform.position.x, doorTransform.position.y - 40, doorTransform.transform.position.z);
+    }
+}
