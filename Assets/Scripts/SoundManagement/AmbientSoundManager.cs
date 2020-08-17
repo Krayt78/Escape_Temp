c53c28f9b9db93ef44a3f64f497fb110b433e36e
tlegorju <thibault.legorju@gmail.com>
2020-08-17 15:23:10 +0200

    new ambient sounds

---
+++ Assets/Scripts/SoundManagement/AmbientSoundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSoundManager : MonoBehaviour
+{
+    public string windPath;
+    private FMOD.Studio.EventInstance windInstance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        windInstance = FMODUnity.RuntimeManager.CreateInstance(windPath);
+        windInstance.start();
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy()
+    {
+        windInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
+}
