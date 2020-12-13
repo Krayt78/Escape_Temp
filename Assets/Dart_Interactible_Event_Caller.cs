29a670699e58a4c192c91152a16a832aa67999ab
Ludovic Domingues <ludovic.domingues96@gmail.com>
2020-12-13 18:31:37 +0100

    Tuto grapplin+dart+controlpanel+door

---
+++ Assets/Dart_Interactible_Event_Caller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Dart_Interactible_Event_Caller : MonoBehaviour
+{
+    //generic script to trigger an event when touched by the dart
+    public UnityEvent myEvent;
+
+    public void OnTouchedByDart()
+    {
+        myEvent.Invoke();
+    }
+
+}
