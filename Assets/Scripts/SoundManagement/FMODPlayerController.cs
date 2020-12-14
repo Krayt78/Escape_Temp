3bab10b996a66fa42cd53079e72b085f9bc5e8c7
TLeGorju <thibault.legorju@gmail.com>
2020-12-14 15:28:14 +0100

    event on voice finished

--- Assets/Scripts/SoundManagement/FMODPlayerController.cs
+++ Assets/Scripts/SoundManagement/FMODPlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +7,7 @@ public class FMODPlayerController
 {
     //Temp
     private static FMOD.Studio.EventInstance playingVoice;
-
+    public event Action OnVoiceFinishedPlaying = delegate{};
 
     public static void PlayOnShotSound(string path, Vector3 position)
     {
@@ -43,5 +44,23 @@ public class FMODPlayerController
         playingVoice.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
 
         playingVoice = PlaySoundInstance(path, pos);
+
+        float eventLength = GetEventLenghtInSeconds(playingVoice);
+        Invoke("CallOnVoiceFinishedPlaying", eventLength);
+    }
+
+    private void CallOnVoiceFinishedPlaying()
+    {
+        OnVoiceFinishedPlaying();
+    }
+
+    public static float GetEventLenghtInSeconds(FMOD.Studio.EventInstance instance)
+    {
+        int length;
+        FMOD.Studio.EventDescription desc;
+        instance.getDescription(out desc);
+        desc.getLength(out length);
+
+        return ((float)length) / 1000.0f;
     }
 }
