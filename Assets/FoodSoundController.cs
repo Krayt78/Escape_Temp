7e2ae59c5df69dd40de10dcce7478c6ea3555dd7
tlegorju <thibault.legorju@mainbot.me>
2021-03-01 15:22:22 +0100

    Food sound

---
+++ Assets/FoodSoundController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSoundController : MonoBehaviour
+{
+    public string FoodIdleEvent;
+    [SerializeField] Rigidbody rb;
+
+    FMOD.Studio.EventInstance soundInstance;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        soundInstance = FMODPlayerController.PlaySoundAttachedToGameObject(FoodIdleEvent, rb);   
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        soundInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        soundInstance.release();
+    }
+
+    private void OnDestroy()
+    {
+        soundInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        soundInstance.release();
+    }
+}
