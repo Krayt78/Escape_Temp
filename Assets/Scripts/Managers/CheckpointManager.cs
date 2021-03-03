1e079fcfb4567dcdd880938468db54d649917795
NolanGougeon <gnolan@hotmail.fr>
2021-03-03 16:54:24 +0100

    Système de checkpoint

---
+++ Assets/Scripts/Managers/CheckpointManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    private static CheckpointManager instance = null;
+    public static CheckpointManager Instance { get { return instance; } }
+
+    private Transform lastCheckpoint;
+
+    [SerializeField] private Transform firstCheckpoint;
+    [SerializeField] private Transform playerTransform;
+
+    private bool transformIsReseting;
+
+    private void Awake()
+    {
+        if (instance)
+        {
+            Destroy(this);
+            return;
+        }
+        instance = this;
+        transformIsReseting = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastCheckpoint = firstCheckpoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("m") && !transformIsReseting)
+        {
+            transformIsReseting = true;
+            Time.timeScale = 0;
+            GoToLastCheckpoint();
+        }
+    }
+
+    public void GoToLastCheckpoint()
+    {
+        playerTransform.position = lastCheckpoint.position;
+        playerTransform.rotation = lastCheckpoint.rotation;
+        Time.timeScale = 1;
+        transformIsReseting = false;
+    }
+
+    public void setCheckPoint(Transform newCheckPoint)
+    {
+        lastCheckpoint = newCheckPoint;
+    }
+}
