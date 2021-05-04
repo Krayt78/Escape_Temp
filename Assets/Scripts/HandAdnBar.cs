c69a217928bb4e6ae9d8dcf37a500bb3d9aaefc7
LUDO\Ludovic <ludovic.domingues96@gmail.com>
2021-05-04 15:59:40 +0200

    added dna ui on right hand

---
+++ Assets/Scripts/HandAdnBar.cs
@@ -0,0 +1,26 @@
+﻿using SDD.Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandAdnBar : MonoBehaviour
+{
+    [SerializeField] Image AdnImage;
+    public PlayerDNALevel playerDNALevel;
+    private float AdnRedTreshold = .3f;
+
+    private void Start() {
+        playerDNALevel.OnDnaLevelChanged += OnDnaLevelChanged;
+    }
+
+    //Can be optimised by using bool on the colours instead 
+    private void OnDnaLevelChanged(float e) {
+        Debug.Log(e);
+        float percentage = e;
+
+            AdnImage.fillAmount = e;
+            AdnImage.color = Color.blue;
+
+    }
+}
