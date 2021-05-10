a16b7f445d6235698b587d16b18b38d6f04285e8
LUDO\Ludovic <ludovic.domingues96@gmail.com>
2021-05-10 14:09:15 +0200

    added fade to xr

---
+++ Assets/LevelFade.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public  class LevelFade : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public void FadeIn() {
+        animator.SetTrigger("FadeIn");
+    }
+
+    public void FadeOut() {
+        animator.SetTrigger("FadeOut");
+    }
+
+}
